8342e0e67501f79b157a77826a377aba327dcca4
Daniel Nolan <11509311+doolan87@users.noreply.github.com>
2020-07-29 21:41:23 +0100

    add example vnet resource

---
+++ modules/vnet/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_virtual_network" "example" {
+  name                = "virtualNetwork1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "Production"
+  }
+}
